c9d1e3b76094aca09da68eaea89f3b352efe2a2e
timad <timad@INBOOK_X1>
2023-03-07 11:21:34 +0100

    Add project files.

---
+++ FormApp/Controllers/PersonController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FormApp.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Submit(string name, string address, string email)
+        {
+            ViewData["name"] = name;
+            ViewData["address"] = address;
+            ViewData["email"] = email;
+            return View();
+        }
+    }
+}
---
+++ FormApp/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace FormApp.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public string? Email { get; set; }
+    }
+}
